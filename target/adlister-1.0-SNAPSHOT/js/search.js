c7c50df778c6268e461a97d975e021586eb106b7
AlvinMckenzie <112984252+AlvinMckenzie@users.noreply.github.com>
2023-01-12 02:31:44 -0600

    adding css to default & search views

--- target/adlister-1.0-SNAPSHOT/js/search.js
+++ target/adlister-1.0-SNAPSHOT/js/search.js
@@ -1,5 +1,5 @@
-(()=>{
-    const default_view = document.getElementById('default_view')
+(async ()=>{
+    // const default_view = document.getElementById('default_view')
     const search_view = document.getElementById('search_view')
     const searchinput = document.getElementById("searchTerm")
 
@@ -11,13 +11,15 @@
         //Iterate through the new results
         for(let item in response){
             const listing = response[item]
-            const title = listing.title;
-            const description = listing.description;
-            const image = listing.image;
+            // const title = listing.title;
+            // const description = listing.description;
+            // const image = listing.image;
 
             //Generate a listing container
             const listingContainer = document.createElement('div')
-            listingContainer.classList.add('col-6','col-md-4','col-lg-3')
+            console.log(listing)
+            // listingContainer.classList.add('col-6','col-md-4','col-lg-3')
+            // listingContainer.style.display = 'inline-block'
 
             const listingTitle = document.createElement('h2')
             listingTitle.innertText = listing.title
@@ -26,11 +28,11 @@
             listingImageLink.href = `/listing?id=${listing.id}`
 
             const listingImage = document.createElement('img')
-            listingImage.classList.add('img-fluid','img-thumbnail')
+            // listingImage.classList.add('img-fluid','img-thumbnail')
             listingImage.src = listing.image
 
-            const descriptionParagraph = document.createElement('p')
-            descriptionParagraph.innerText = listing.description
+            // const descriptionParagraph = document.createElement('p')
+            // descriptionParagraph.innerText = listing.description
 
             listingContainer.appendChild(listingTitle);
 
@@ -38,19 +40,32 @@
 
             listingContainer.appendChild(listingImageLink);
 
-            listingContainer.appendChild(descriptionParagraph);
+            // listingContainer.appendChild(descriptionParagraph);
 
             //Attach it to the search_view
             search_view.appendChild(listingContainer);
         }
     }
 
-    searchinput.onkeyup = async (e) => {
+    const doSearch = async (e) => {
         const searchTerm = e.target.value
 
         if(searchTerm.length === 0){
-            default_view.style.display = 'block'
-            search_view.style.display = 'none'
+            console.log(searchTerm)
+            const request = await fetch("http://localhost:8080/search", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    searchTerm: ''
+                })
+            })
+            const response = await request.json()
+            console.log(response)
+            // default_view.style.display = 'none !important'
+            search_view.style.display = 'grid !important'
+            populateResults(response)
         }else {
             console.log(searchTerm)
             const request = await fetch("http://localhost:8080/search", {
@@ -64,12 +79,13 @@
             })
             const response = await request.json()
             console.log(response)
-            default_view.style.display = 'none'
-            search_view.style.display = 'block'
+            // default_view.style.display = 'none !important'
+            search_view.style.display = 'grid !important'
             populateResults(response)
         }
     }
-
+    searchinput.onkeyup = doSearch
+    await doSearch({target: {value: ''}})
 
 
 })()
