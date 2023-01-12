b9e0a8f52808dab6bfc8ee26f73de43af5124e5b
AlvinMckenzie <112984252+AlvinMckenzie@users.noreply.github.com>
2023-01-12 10:59:08 -0600

    continued styling search_view

--- src/main/webapp/js/search.js
+++ src/main/webapp/js/search.js
@@ -15,24 +15,52 @@
             // const description = listing.description;
             // const image = listing.image;
 
+            //lets see
+            const cardDiv = document.createElement("div")
+            cardDiv.class = "card"
+
+            const cardBody = document.createElement("div")
+            cardBody.classList.add("card-body", "d-flex", "flex-column", "align-items-center")
+
+            const cardFooter = document.createElement("div")
+            cardFooter.classList.add("card-header", "d-flex", "justify-content-between")
+
+            const positionDiv = document.createElement("div")
+            positionDiv.classList.add("d-flex", "justify-content-between", "align-items-start")
+
+
+
+
+            //end of lets see
+
+
             //Generate a listing container
             const listingContainer = document.createElement('div')
             console.log(listing)
-            // listingContainer.classList.add('col-6','col-md-4','col-lg-3')
-            // listingContainer.style.display = 'inline-block'
+            listingContainer.classList.add('col-6','col-md-4','col-lg-3')
+            listingContainer.style.display = 'inline-block'
 
             const listingTitle = document.createElement('h2')
-            listingTitle.innertText = listing.title
-
-            const listingImageLink = document.createElement('a')
-            listingImageLink.href = `/listing?id=${listing.id}`
+            listingTitle.innerText = listing.title
+            console.log(listing.title)
 
             const listingImage = document.createElement('img')
-            // listingImage.classList.add('img-fluid','img-thumbnail')
+            listingImage.classList.add('img-fluid')
             listingImage.src = listing.image
+
+            const listingImageLink = document.createElement('h2')
+            listingImageLink.href = `/listing?id=${listing.id}`
 
             // const descriptionParagraph = document.createElement('p')
             // descriptionParagraph.innerText = listing.description
+
+
+            cardBody.appendChild(positionDiv)
+            cardDiv.appendChild(cardFooter)
+            cardDiv.appendChild(cardBody)
+            listingContainer.appendChild(cardDiv)
+
+
 
             listingContainer.appendChild(listingTitle);
 
@@ -45,6 +73,7 @@
             //Attach it to the search_view
             search_view.appendChild(listingContainer);
         }
+
     }
 
     const doSearch = async (e) => {
