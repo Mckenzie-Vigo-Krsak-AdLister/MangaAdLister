6ae0d1022004f5366c8ba5651a61fc4367c9c46b
Aldanis Vigo <aldanisvigo@gmail.com>
2023-01-12 12:41:57 -0600

    cart working

--- target/adlister-1.0-SNAPSHOT/Js/listing.js
+++
@@ -1,7 +0,0 @@
-(()=>{
-    // const addToCartBtn = document.getElementById('addToCart')
-    // addToCartBtn.onclick = function(e){
-    //     const listing = this.getAttribute('listing');
-    //     console.log(listing)
-    // }
-})()
